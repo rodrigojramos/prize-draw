bd57dbaf4a0c6f4ed8c1e3e568c912ce6cc98e4b
Rodrigo Ramos <digojramos@gmail.com>
2024-11-12 17:19:06 -0300

    Project created, Docker-compose, configuration MongoDB

---
+++ prize-draw/src/main/java/com/rodrigoramos/prize_draw/PrizeDrawApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoramos.prize_draw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrizeDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrizeDrawApplication.class, args);
+	}
+
+}
---
+++ prize-draw/src/test/java/com/rodrigoramos/prize_draw/PrizeDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoramos.prize_draw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrizeDrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
