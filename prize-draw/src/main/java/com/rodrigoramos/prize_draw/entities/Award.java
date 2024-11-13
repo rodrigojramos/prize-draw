45503ac6c595ad70197021cd3a0f662a6911bba7
Rodrigo Ramos <digojramos@gmail.com>
2024-11-13 08:23:25 -0300

    Award

---
+++ prize-draw/src/main/java/com/rodrigoramos/prize_draw/entities/Award.java
@@ -0,0 +1,38 @@
+package com.rodrigoramos.prize_draw.entities;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "awards")
+public class Award {
+
+    @Id
+    private ObjectId id;
+
+    private String name;
+
+    public Award() {
+    }
+
+    public Award(ObjectId id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
