7aec7aea27e7ef926d7fb572ec88a7dd53e1eb7b
Rodrigo Ramos <digojramos@gmail.com>
2024-11-13 08:18:41 -0300

    Participant

---
+++ prize-draw/src/main/java/com/rodrigoramos/prize_draw/entities/Participant.java
@@ -0,0 +1,57 @@
+package com.rodrigoramos.prize_draw.entities;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "participants")
+public class Participant {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    private String document;
+    private String email;
+
+    public Participant() {
+    }
+
+    public Participant(ObjectId id, String name, String document, String email) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.email = email;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
