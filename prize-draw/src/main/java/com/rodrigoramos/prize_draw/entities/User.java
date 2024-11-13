17b48fe92fe6fb731594bf3bd2bad9e994472c6e
Rodrigo Ramos <digojramos@gmail.com>
2024-11-13 08:11:11 -0300

    User

---
+++ prize-draw/src/main/java/com/rodrigoramos/prize_draw/entities/User.java
@@ -0,0 +1,67 @@
+package com.rodrigoramos.prize_draw.entities;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    private String document;
+    private String email;
+    private Integer quantityPrizeDraw;
+
+    public User() {
+    }
+
+    public User(ObjectId id, String name, String document, String email, Integer quantityPrizeDraw) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.email = email;
+        this.quantityPrizeDraw = quantityPrizeDraw;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getQuantityPrizeDraw() {
+        return quantityPrizeDraw;
+    }
+
+    public void setQuantityPrizeDraw(Integer quantityPrizeDraw) {
+        this.quantityPrizeDraw = quantityPrizeDraw;
+    }
+}
---
+++ prize-draw/src/main/java/com/rodrigoramos/prize_draw/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rodrigoramos.prize_draw.repositories;
+
+import com.rodrigoramos.prize_draw.entities.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
